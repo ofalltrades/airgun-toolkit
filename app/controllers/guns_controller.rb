865e35b5fb2d8671b0d26876899dee8c9b5b486c
Jake Johnson <kognizant.kog@gmail.com>
2017-07-26 22:28:27 -0400

    small changes to controllers, nothing important yet, just experimenting

--- app/controllers/guns_controller.rb
+++ app/controllers/guns_controller.rb
@@ -1,18 +1,10 @@
 class GunsController < ApplicationController
-  before_action :gun, except [:create]
+  before_action :gun, except :create
 
   def create
     render :create if Gun.new(gun_params).save
     head :internal_server_error
   end
-
-  # def index
-  #   render :index
-  # end
-
-  # def show
-  #   render :show
-  # end
 
   def update
     gun.update_attributes(gun_params.without(:ballistic_data))
@@ -20,8 +12,6 @@ class GunsController < ApplicationController
     if gun_params[:ballistic_data]
       gun.ballistic_data.push(BallisticData.new(gun_params.slice(:ballistic_data)))
     end
-
-    render :update
   end
 
   private
@@ -41,6 +31,6 @@ class GunsController < ApplicationController
   end
 
   def gun
-    @gun || Gun.find gun_params[:id]
+    @gun ||= Gun.find gun_params[:id]
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,29 @@
 class UsersController < ApplicationController
-  def create; end
-  def show; end
-  def destroy; end
-  def edit; end
+  before_action :user, except: :index
+
+  def index
+    respond_to do |format|
+      format.html
+      format.json { @users = User.all }
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  private
+
+  def gun_params
+    params.require(:data).permit(
+      :id
+    )
+  end
+
+  def user
+    @user ||= User.fine params[:id]
+  end
 end
