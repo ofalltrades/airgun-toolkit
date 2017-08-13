c7a0f80cca6c474f68024691b53a6760edf2ea2b
Jake Johnson <kognizant.kog@gmail.com>
2017-08-12 23:46:53 -0400

    just some boilerplate for toy api work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   resources :users, :guns, :pellets
   root 'users#show'
+
+  namespace :api, defaults: { format: :json },
+                  constraints: { subdomain: 'api' },
+                  path: '/' do                       # set starting paths for
+                                                     #   requests to root in
+                                                     #   relation to the subdomain
+    scope module: :v0_1_0 do
+
+    end
+  end
 end
---
+++ spec/controllers/api/v0_1_0/api_constraints_spec.rb
@@ -0,0 +1,3 @@
+describe ApiConstraints do
+
+end
